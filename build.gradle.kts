8755946f0891ebd76870027f54795b31d96e2ca1
theapache64 <theapache64@gmail.com>
2025-04-05 19:25:25 +0530

    🌛 first commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+// Add compose gradle plugin
+plugins {
+    kotlin("multiplatform") version "2.0.20"
+    kotlin("plugin.compose") version "2.0.20"
+    id("org.jetbrains.compose") version "1.7.0-alpha03"
+}
+group = "io.github.theapache64.composedaisyui"
+version = "1.0.0-alpha01"
+
+// Add maven repositories
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.html.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+// Workaround for https://youtrack.jetbrains.com/issue/KT-49124
+rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+    versions.webpackCli.version = "4.10.0"
+}
