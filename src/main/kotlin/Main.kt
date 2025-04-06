0f20337dd8763064b5a53ba1749aa37f5ca9132e
Mahyar <mlk.mahyar@gmail.com>
2025-04-06 22:34:09 +0330

    Migrate to tailwind v4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.attributes.AttrsScope
 import org.jetbrains.compose.web.css.padding
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Button
@@ -9,42 +10,46 @@ import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposableInBody
-
+import org.w3c.dom.Element
 
 fun main() {
     renderComposableInBody {
         var count by mutableStateOf(0)
-        Div({ classes("container", "mx-auto", "bg-red-700") }) {
+        Div({ tw("container mx-auto bg-blue-400") }) {
             // Content
             H1 {
-                Text("Hello World!")
+                Text("Hello from Tailwind v4")
             }
         }
 
-        Button(
-            attrs = {
-                classes("btn", "btn-primary")
-            }
-        ) {
+        Button({ tw("btn btn-primary") }) {
             Text("Hello DaisyUI!")
         }
 
         Div({ style { padding(25.px) } }) {
-            Button(attrs = {
+            Button({
+                tw("btn btn-active btn-secondary")
                 onClick { count -= 1 }
             }) {
                 Text("-")
             }
 
-            Span({ style { padding(15.px) } }) {
+            Span({ tw("p-8") }) {
                 Text("$count")
             }
 
-            Button(attrs = {
+            Button({
+                tw("btn btn-active btn-secondary")
                 onClick { count += 1 }
             }) {
                 Text("+")
             }
         }
     }
+}
+
+// Utility function for copy-pasting tailwind string classes
+fun AttrsScope<Element>.tw(`class`: String) {
+    `class`.ifBlank { return }
+    classes(`class`.trim().split(" "))
 }
